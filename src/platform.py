d68a3b7ed95552859b97e09b845df5f27bceaf8b
christopher simak <chris@christophers-MacBook-Air.local>
2019-11-26 13:34:25 -0500

    hero sequence

--- src/platform.py
+++ src/platform.py
@@ -4,15 +4,9 @@ import pygame
 
 class Platform(pygame.sprite.Sprite):
     def __init__(self, width, height, pos_x, pos_y, color):
+        pygame.sprite.Sprite.__init__(self)
         self.width, self.height = width, height
         self.pos_x, self.pos_y = pos_x, pos_y
-        pygame.sprite.Sprite.__init__(self)
         self.surface = pygame.surface.Surface((self.width, self.height))
         self.rect = self.surface.get_rect()
         self.surface.fill(color)
-
-
-
-
-
-
