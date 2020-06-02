deeee01d423abf7d1f8d4956cf2e5458550a1098
christopher simak <chris@christophers-MacBook-Air.local>
2020-06-01 23:04:23 -0400

    updates

--- src/controller.py
+++ src/controller.py
@@ -10,25 +10,22 @@ from src import coin
 
 class Controller:
     def __init__(self, width=800, height=400):
+        pygame.init()
         self.screen = pygame.display.set_mode((width, height))
-        self.background = pygame.Surface(self.screen.get_size()).convert()
-        self.background_pic = 'assets/Sprites/space.png'
+        self.background =
+        self.background_pic = pygame.image.load('assets/Sprites/space.png')
         self.state = "BEGIN"
-        self.height = height
+        self.hero_state = "RUN"
         self.width = width
+        self.height = height
+        self.white = (255,255,255)
         self.jump = True
         self.run = True
         self.hero = hero.Hero("Johnny", self.width / 3, self.height / 3, "assets/Sprites/run 1.png", "right", "RUN")
-        self.obstacles = pygame.sprite.Group()
         self.wall = wall.Wall(self.width / 4, self.height - 240, 'assets/Sprites/stoneWall.png')
-        # add the obstacles into the group here
-        self.white = (255, 255, 255)
-        self.red = (255, 0, 0)
-        self.black = (0, 0, 0)
-        self.coins = pygame.sprite.Group(coin.Coin(self.width / 5, self.height - 240, 'assets/Sprites/goldCoin1.png'))
-        self.bullets = pygame.sprite.Group()
-        self.all_sprites = pygame.sprite.Group((self.hero,) + tuple(self.obstacles))
-        self.score = pygame.time.Clock()
+        self.coin = coin.Coin(self.width / 5, self.height - 240, 'assets/Sprites/goldCoin1.png')
+        self.bullet = bullet.Bullet(self.hero.rect.centerx, self.hero.rect.centery,self.hero.direction,"assets/Sprites/bullet.png")
+
 
     def mainLoop(self):
         '''
@@ -50,7 +47,6 @@ class Controller:
         :param = None
         :returns = None
         '''
-        self.hero.kill()
         background = pygame.image.load(self.background_pic)
         background_size = self.screen.get_size()
         background_rect = background.get_rect()
@@ -63,130 +59,22 @@ class Controller:
         background_screen.blit(name_of_game, ((self.width / 3) + 50, self.height / 4))
         background_screen.blit(instructions, ((self.width / 3) - 220, self.height / 1.5))
         pygame.display.flip()
-        while self.run:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        self.state = "GAME"
-                        self.mainLoop()
-
-    def gameOverScreen(self):
-        '''
-        this method creates a game over screen.
-        :param = None
-        :returns = None
-        '''
-        self.hero.kill()
-        background = pygame.image.load(self.background_pic)
-        # will get size of background image
-        # if background_screen doesn't work, change to self.screen
-        background_size = self.screen.get_size()
-        background_rect = background.get_rect()
-        background_screen = pygame.display.set_mode(background_size)
-        background_screen.blit(background, background_rect)
-        my_font = pygame.font.SysFont(None, 30)
-        message = my_font.render('Game Over, Press space to play again.', False, (255, 255, 255))
-        background_screen.blit(message, ((self.width / 3) - 50, self.height / 3))
-        pygame.display.flip()
-        while self.state == "LOSE":
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        self.state = 'GAME'
-                        self.mainLoop()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.state = "GAME"
+                    self.mainLoop()
 
     def gameLoop(self):
-        '''
-        this method runs the actual game. It also checks for all the game events
-        :param = None
-        :returns = None
-        '''
-        pygame.time.delay(100)
-        background = pygame.image.load(self.background_pic)
-        background_size = self.screen.get_size()
-        background_rect = background.get_rect()
-        background_screen = pygame.display.set_mode(background_size)
-        background_screen.blit(background, background_rect)
         while self.state == "GAME":
-
-            self.side_Scroller()
-            self.hero.run()
-
-            # self.side_Scroller()
-            self.hero.run()
-
-            if random.randrange(4) == 0:
-                self.obstacles.add(spikes.Spikes(self.width / 3, self.height / 3, 'assets/Sprites/spike.png'),
-                                   wall.Wall(self.width / 4, self.height / 4, 'assets/Sprites/stoneWall.png'),
-                                   coin.Coin(self.width / 5, self.height / 5, 'assets/Sprites/goldCoin1.png'))
-                self.all_sprites.draw(self.screen)
-                pygame.display.flip()
+            background = pygame.image.load(self.background_pic)
+            background_size = self.screen.get_size()
+            background_rect = background.get_rect()
+            background_screen = pygame.display.set_mode(background_size)
+            background_screen.blit(background, background_rect)
+            for img in self.hero.run_sprite:
 
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE or pygame.K_UP:
-                        self.hero.jump()
-                    elif event.key == pygame.K_z:
-                        b = bullet.Bullet(self.hero.rect.centerx, self.hero.rect.centery, "right",
-                                          "assets/Sprites/bullet.png")
-                        self.bullets.add(b)
-                        self.bullets.update()
-
-            get_coin = pygame.sprite.spritecollide(self.hero, self.coins, True)
-            bullet_collides = pygame.sprite.spritecollide(self.wall, self.bullets, True)
-            collides = pygame.sprite.spritecollide(self.hero, self.obstacles, True)
-            if collides:
-                self.state = "LOSE"
-            elif bullet_collides:
-                self.wall.kill()
-            elif get_coin:
-                self.coins.kill()
-                self.score += 10
-
-            self.bullets.update()
-            self.screen.blit(self.background, (0, 0))
-            self.screen.blit(self.hero.image, (self.hero.rect.x, self.hero.rect.y))
-            self.all_sprites.draw(self.screen)
-            pygame.display.flip()
-
-    def side_Scroller(self):
-        '''
-        this method continuously scrolls the screen as the game runs.
-        :param = None
-        :returns = None
-        '''
-        background = pygame.image.load('assets/Sprites/space.png')
-        # will get size of background image
-        # may need to go inside gameLoop()
-        background_size = background.get_size()
-        background_rect = background.get_rect()
-        screen = pygame.display.set_mode(background_size)
-        w, h = background_size
-        x = 0
-        y = 0
-
-        x1 = 0
-        y1 = -h
-        run = True
-
-        while run:
-            screen.blit(background, background_rect)
-            pygame.display.flip()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            y1 += 5
-            y += 5
-            screen.blit(background, (x, y))
-            screen.blit(background, (x1, y1))
-            if y > h:
-                y = -h
-            elif y1 > h:
-                y1 = h
-            pygame.display.flip()
+                pass
--- src/hero.py
+++ src/hero.py
@@ -1,8 +1,9 @@
 import pygame
+from src import controller
 
 
 class Hero(pygame.sprite.Sprite):
-    def __init__(self, name, x, y, image,direction, state):
+    def __init__(self, name, x, y, image,direction):
         pygame.sprite.Sprite.__init__(self)
         self.name = name
         self.image = pygame.image.load(image).convert_alpha()
@@ -10,14 +11,16 @@ class Hero(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
         self.direction = direction
+        self.obj = controller.Controller()
+        self.state = self.obj.hero_state
+        self.screen = self.obj.screen
         self.run_sprite = ["assets/Sprites/run 1.png", "assets/Sprites/run 2.png", "assets/Sprites/run 3.png",
                            "assets/Sprites/run 4.png", "assets/Sprites/run 5.png", "assets/Sprites/run 6.png"]
         self.run_index = 0
 
-        self.character_state = state
         self.jump_sprite = ["assets/Sprites/jump1.png"*10,"assets/Sprites/jump2.png"*10]
 
-        self.state = state
+
         self.jump_sprite = ["assets/Sprites/jump1.png", "assets/Sprites/jump2.png"]
 
         self.jump_index = 0
@@ -25,7 +28,6 @@ class Hero(pygame.sprite.Sprite):
                                  "assets/Sprites/runshoot3.png", "assets/Sprites/runshoot4.png",
                                  "assets/Sprites/runshoot5.png", "assets/Sprites/runshoot6.png"]
         self.run_shoot_index = 0
-        self.current_state = ""
 
 
     def run(self):
@@ -36,11 +38,16 @@ class Hero(pygame.sprite.Sprite):
         '''
         x = self.rect.x
         y = self.rect.y
-        self.image = pygame.image.load(self.run_sprite[self.run_index]).convert_alpha()
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.run_index = (self.run_index+1) % len(self.run_sprite)
+        while self.state == "RUN":
+            self.image = pygame.image.load(self.run_sprite[self.run_index]).convert_alpha()
+            self.rect = self.image.get_rect()
+            self.rect.x = x
+            self.rect.y = y
+            self.run_index = (self.run_index+1) % len(self.run_sprite)
+
+
+
+
 
     def jump(self):
         '''
@@ -50,17 +57,18 @@ class Hero(pygame.sprite.Sprite):
         '''
         x = self.rect.x
         y = self.rect.y
-        self.image = pygame.image.load(self.jump_sprite[self.jump_index]).convert_alpha()
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.jump_index = (self.jump_index+1) % len(self.jump_sprite)
-        if self.jump_index < 20:
-            self.rect.y -= 10
-        elif self.jump_index < 40:
-            self.rect.y += 10
-        else:
-            self.state = "RUN"
+        while self.state == "JUMP":
+            self.image = pygame.image.load(self.jump_sprite[self.jump_index]).convert_alpha()
+            self.rect = self.image.get_rect()
+            self.rect.x = x
+            self.rect.y = y
+            self.jump_index = (self.jump_index+1) % len(self.jump_sprite)
+            if self.jump_index < 10:
+                self.rect.y -= 10
+            elif self.jump_index > 10:
+                self.rect.y += 10
+            else:
+                self.state = "RUN"
 
     def run_shoot(self):
         '''
@@ -75,21 +83,6 @@ class Hero(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
         self.run_shoot_index = (self.run_shoot_index+1) % len(self.run_shoot_sprite)
-
-
-    def update(self, state):
-        '''
-        this method checks the state of the hero and updates it accordingly.
-        :param = state
-        :returns = None
-        '''
-        self.state = state
-        if self.state == "RUN":
-            self.run()
-        elif self.state == "JUMP":
-            self.jump()
-        elif self.state == "RUNSHOOT":
-            self.run_shoot()
 
     def position(self):
         '''
